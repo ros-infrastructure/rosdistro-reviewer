00e4e9bd4a733ef1f0306f3308cff2d92e311323
Scott K Logan <logans@cottsay.net>
2024-08-02 13:38:27 -0500

    Add top-level executable for rosdistro-reviewer (#6)

---
+++ rosdistro_reviewer/__main__.py
@@ -0,0 +1,9 @@
+# Copyright 2024 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+import sys
+
+from rosdistro_reviewer.command import main
+
+
+sys.exit(main())
