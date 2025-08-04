e9d3ba1369705ab2c9bf71c39c21f5f934c4fec0
Scott K Logan <logans@cottsay.net>
2025-08-04 18:04:34 -0500

    Generate package documentation using Sphinx (#38)
    This includes the project README as well as the API documentation.

---
+++ doc/conf.py
@@ -0,0 +1,39 @@
+# Copyright 2025 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'rosdistro-reviewer'
+copyright = '2025 Open Source Robotics Foundation, Inc'
+author = 'Scott K Logan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinxcontrib.apidoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for API documentation -------------------------------------------
+# https://github.com/sphinx-contrib/apidoc?tab=readme-ov-file#configuration
+
+apidoc_module_dir = '../rosdistro_reviewer'
+apidoc_toc_file = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_domain_indices = False
+html_use_index = False
---
+++ doc/index.rst
@@ -0,0 +1,8 @@
+.. include:: ../README.rst
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+   :caption: API Documentation
+
+   api/rosdistro_reviewer
