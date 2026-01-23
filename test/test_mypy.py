64150be74d21cfa9429ae340fbbdd088e4992721
Scott K Logan <logans@cottsay.net>
2026-01-23 12:58:32 -0600

    Handle optional PyGithub dependency more gracefully (#63)
    Specifically:
    1. Appropriately note what versions we can actually use
    2. Don't enforce mypy checks when we don't have a compatible version

--- test/test_mypy.py
+++ test/test_mypy.py
@@ -8,12 +8,39 @@ import sys
 import pytest
 
 
+def _have_compatible_github():
+    """
+    Determine if a compatible version of PyGithub is available.
+
+    Of note, PyGithub doesn't expose a __version__ attribute. We could try to
+    figure it out by performing package discovery, but we probably couldn't
+    parse and compare the version numbers without taking a new dependency,
+    which feels like a lot of work just to avoid running mypy on older
+    platforms...
+    """
+    try:
+        from github import Auth  # noqa: F401
+        from github import Github  # noqa: F401
+        from github.PullRequest import ReviewComment  # noqa: F401
+    except ImportError:
+        return False
+    return True
+
+
 @pytest.mark.linter
 def test_mypy() -> None:
+    exclude = []
+    if not _have_compatible_github():
+        exclude.append(r'/submitter/github\.py$')
+
     result = subprocess.run(
         [
             sys.executable, '-m', 'mypy',
             '--namespace-packages', '--explicit-package-bases', '.',
+        ] + [
+            arg
+            for pattern in exclude
+            for arg in ('--exclude', pattern)
         ],
         cwd=os.path.dirname(os.path.dirname(__file__)),
         check=False,
